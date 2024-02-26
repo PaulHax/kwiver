a6d8cde2ab3da69606e3d440f96c6c5f41f6022d
Paul Beasly <paul.beasly@kitware.com>
2024-02-26 13:54:04 -0500

    Update close_loops_bad_frames_only

--- arrows/core/close_loops_bad_frames_only.cxx
+++ arrows/core/close_loops_bad_frames_only.cxx
@@ -26,84 +26,20 @@ namespace core {
 
 using namespace kwiver::vital;
 
-/// Default Constructor
-close_loops_bad_frames_only
-::close_loops_bad_frames_only()
-  : enabled_( true ),
-    percent_match_req_( 0.35 ),
-    new_shot_length_( 2 ),
-    max_search_length_( 5 )
-{}
-
 // ----------------------------------------------------------------------------
-vital::config_block_sptr
+void
 close_loops_bad_frames_only
-::get_configuration() const
+::initialize()
 {
-  // get base config from base class
-  kwiver::vital::config_block_sptr config = algorithm::get_configuration();
-
-  // Sub-algorithm implementation name + sub_config block
-  // - Feature Matcher algorithm
-  algo::match_features::get_nested_algo_configuration(
-    "feature_matcher",
-    config, matcher_ );
-
-  // Bad frame detection parameters
-  config->set_value(
-    "enabled", enabled_,
-    "Should bad frame detection be enabled? This option will attempt to "
-    "bridge the gap between frames which don't meet certain criteria "
-    "(percentage of feature points tracked) and will instead attempt "
-    "to match features on the current frame against past frames to "
-    "meet this criteria. This is useful when there can be bad frames." );
-
-  config->set_value(
-    "percent_match_req", percent_match_req_,
-    "The required percentage of features needed to be matched for a "
-    "stitch to be considered successful (value must be between 0.0 and "
-    "1.0)." );
-
-  config->set_value(
-    "new_shot_length", new_shot_length_,
-    "Number of frames for a new shot to be considered valid before "
-    "attempting to stitch to prior shots." );
-
-  config->set_value(
-    "max_search_length", max_search_length_,
-    "Maximum number of frames to search in the past for matching to "
-    "the end of the last shot." );
-
-  return config;
+  attach_logger( "arrows.core.close_loops_bad_frames_only" );
 }
 
 // ----------------------------------------------------------------------------
-/// Set this algo's properties via a config block
 void
 close_loops_bad_frames_only
-::set_configuration( vital::config_block_sptr in_config )
+::set_configuration_internal( [[maybe_unused]] vital::config_block_sptr config )
 {
-  // Starting with our generated config_block to ensure that assumed values are
-  // present
-  // An alternative is to check for key presence before performing a get_value()
-  // call.
-  vital::config_block_sptr config = this->get_configuration();
-  config->merge_config( in_config );
-
-  // Setting nested algorithm instances via setter methods instead of directly
-  // assigning to instance property.
-  algo::match_features_sptr mf;
-  algo::match_features::set_nested_algo_configuration(
-    "feature_matcher",
-    config, mf );
-  matcher_ = mf;
-
-  // Settings for bad frame detection
-  enabled_ = config->get_value< bool >( "enabled" );
-  percent_match_req_ = config->get_value< double >( "percent_match_req" );
-  max_search_length_ = config->get_value< unsigned >( "max_search_length" );
-  new_shot_length_ = config->get_value< unsigned >( "new_shot_length" );
-  new_shot_length_ = ( new_shot_length_ ? new_shot_length_ : 1 );
+  c_new_shot_length = ( c_new_shot_length ? c_new_shot_length : 1 );
 }
 
 // ----------------------------------------------------------------------------
@@ -112,7 +48,7 @@ close_loops_bad_frames_only
 ::check_configuration( vital::config_block_sptr config ) const
 {
   return (
-    algo::match_features::check_nested_algo_configuration(
+    check_nested_algo_configuration< algo::match_features >(
       "feature_matcher",
       config )
     &&
@@ -131,23 +67,23 @@ close_loops_bad_frames_only
   vital::image_container_sptr ) const
 {
   // check if enabled and possible
-  if( !enabled_ || frame_number <= new_shot_length_ )
+  if( !c_enabled || frame_number <= c_new_shot_length )
   {
     return input;
   }
 
   // check if we should attempt to stitch together past frames
   std::vector< vital::track_sptr > all_tracks = input->tracks();
-  vital::frame_id_t frame_to_stitch = frame_number - new_shot_length_ + 1;
+  vital::frame_id_t frame_to_stitch = frame_number - c_new_shot_length + 1;
   double pt = input->percentage_tracked( frame_to_stitch - 1, frame_to_stitch );
-  bool stitch_required = ( pt < percent_match_req_ );
+  bool stitch_required = ( pt < c_percent_match_req );
 
   // confirm that the new valid shot criteria length is satisfied
   vital::frame_id_t frame_to_test = frame_to_stitch + 1;
   while( stitch_required && frame_to_test <= frame_number )
   {
     pt = input->percentage_tracked( frame_to_test - 1, frame_to_test );
-    stitch_required = ( pt >= percent_match_req_ );
+    stitch_required = ( pt >= c_percent_match_req );
     frame_to_test++;
   }
 
@@ -162,9 +98,9 @@ close_loops_bad_frames_only
 
   vital::frame_id_t last_frame_to_test = 0;
 
-  if( frame_to_test > max_search_length_ )
+  if( frame_to_test > c_max_search_length )
   {
-    last_frame_to_test = frame_to_test - max_search_length_;
+    last_frame_to_test = frame_to_test - c_max_search_length;
   }
 
   auto stitch_frame_set = std::make_shared< vital::feature_track_set >(
@@ -176,7 +112,7 @@ close_loops_bad_frames_only
       input->active_tracks( frame_to_test ) );
 
     // run matcher alg
-    vital::match_set_sptr mset = matcher_->match(
+    vital::match_set_sptr mset = c_matcher->match(
       test_frame_set->frame_features( frame_to_test ),
       test_frame_set->frame_descriptors( frame_to_test ),
       stitch_frame_set->frame_features( frame_to_stitch ),
@@ -187,7 +123,7 @@ close_loops_bad_frames_only
                                                        stitch_frame_set->size() );
 
     if( 2 * mset->size() >=
-        static_cast< unsigned >( percent_match_req_ * total_features ) )
+        static_cast< unsigned >( c_percent_match_req * total_features ) )
     {
       // modify track history and exit
       std::vector< vital::track_sptr > test_frame_trks =
--- arrows/core/register_algorithms.cxx
+++ arrows/core/register_algorithms.cxx
@@ -42,6 +42,7 @@
 
 // implementation
 #include <arrows/core/associate_detections_to_tracks_threshold.h>
+#include <arrows/core/close_loops_bad_frames_only.h>
 #include <arrows/core/close_loops_exhaustive.h>
 #include <arrows/core/close_loops_keyframe.h>
 #include <arrows/core/compute_association_matrix_from_features.h>
@@ -253,6 +254,10 @@ register_factories( kwiver::vital::plugin_loader& vpl )
 
   fact = vpl.add_factory< vital::algo::close_loops,
     close_loops_exhaustive >( "exhaustive" );
+  fact->add_attribute( kvpf::PLUGIN_MODULE_NAME, "arrows_core" );
+
+  fact = vpl.add_factory< vital::algo::close_loops,
+    close_loops_bad_frames_only >( "bad_frames_only" );
   fact->add_attribute( kvpf::PLUGIN_MODULE_NAME, "arrows_core" );
 }
 
