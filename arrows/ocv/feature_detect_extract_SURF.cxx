637fa26a3c60165503c62f077b9ee3959f2b46ee
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-02-29 17:09:32 -0500

    ocv: Adapt feature_detect_extract_SURF

--- arrows/ocv/feature_detect_extract_SURF.cxx
+++ arrows/ocv/feature_detect_extract_SURF.cxx
@@ -33,139 +33,66 @@ namespace ocv {
 
 namespace {
 
-/// Common ORB private implementation class
-class priv
+// Create new algorithm instance from current parameters
+cv::Ptr< cv_SURF_t >
+create(
+  double hessian_threshold, int n_octaves, int n_octave_layers,
+  bool extended, bool upright )
 {
-public:
-  // Cosntructor
-  priv()
-    : hessian_threshold( 100 ),
-      n_octaves( 4 ),
-      n_octave_layers( 3 ),
-      extended( false ),
-      upright( false )
-  {}
-
-  // Create new algorithm instance from current parameters
-  cv::Ptr< cv_SURF_t >
-  create() const
-  {
 #if KWIVER_OPENCV_VERSION_MAJOR < 3
-    return cv::Ptr< cv_SURF_t >(
-      new cv_SURF_t(
-        hessian_threshold, n_octaves, n_octave_layers,
-        extended, upright  )
-    );
-#else
-    return cv_SURF_t::create(
+  return cv::Ptr< cv_SURF_t >(
+    new cv_SURF_t(
       hessian_threshold, n_octaves, n_octave_layers,
-      extended, upright );
+      extended, upright  )
+  );
+#else
+  return cv_SURF_t::create(
+    hessian_threshold, n_octaves, n_octave_layers,
+    extended, upright );
 #endif
-  }
-
-#if KWIVER_OPENCV_VERSION_MAJOR < 3
-  // Update algorithm with current parameter
-  void
-  update( cv::Ptr< cv_SURF_t > a ) const
-  {
-    a->set( "hessianThreshold", hessian_threshold );
-    a->set( "nOctaves", n_octaves );
-    a->set( "nOctaveLayers", n_octave_layers );
-    a->set( "extended", extended );
-    a->set( "upright", upright );
-  }
-#endif
-
-  // Update config block with current parameter values
-  void
-  update_config( config_block_sptr config ) const
-  {
-    config->set_value(
-      "hessian_threshold", hessian_threshold,
-      "Threshold for hessian keypoint detector used in SURF" );
-    config->set_value(
-      "n_octaves", n_octaves,
-      "Number of pyramid octaves the keypoint detector will "
-      "use." );
-    config->set_value(
-      "n_octave_layers", n_octave_layers,
-      "Number of octave layers within each octave." );
-    config->set_value(
-      "extended", extended,
-      "Extended descriptor flag (true - use extended "
-      "128-element descriptors; false - use 64-element "
-      "descriptors)." );
-    config->set_value(
-      "upright", upright,
-      "Up-right or rotated features flag (true - do not "
-      "compute orientation of features; false - "
-      "compute orientation)." );
-  }
-
-  // Set current values based on config block
-  void
-  set_config( config_block_sptr config )
-  {
-    hessian_threshold = config->get_value< int >( "hessian_threshold" );
-    n_octaves = config->get_value< int >( "n_octaves" );
-    n_octave_layers = config->get_value< int >( "n_octave_layers" );
-    extended = config->get_value< bool >( "extended" );
-    upright = config->get_value< bool >( "upright" );
-  }
-
-  // Parameters
-  double hessian_threshold;
-  int n_octaves;
-  int n_octave_layers;
-  bool extended;
-  bool upright;
-};
+}
 
 } // end namespace anonymous
 
-class detect_features_SURF::priv
-  : public ocv::priv
-{};
-
-class extract_descriptors_SURF::priv
-  : public ocv::priv
-{};
-
+// -------------------------------------------------------------------
+void
 detect_features_SURF
-::detect_features_SURF()
-  : p_( new priv )
+::initialize()
 {
   attach_logger( "arrows.ocv.SURF" );
-  detector = p_->create();
+  detector = create(
+    get_hessian_threshold(), get_n_octaves(), get_n_octaves_layers(),
+    get_extended(), get_upright() );
 }
 
 detect_features_SURF
 ::~detect_features_SURF()
 {}
 
-vital::config_block_sptr
+void
 detect_features_SURF
-::get_configuration() const
+::set_configuration_internal( VITAL_UNUSED vital::config_block_sptr config )
 {
-  config_block_sptr config = detect_features::get_configuration();
-  p_->update_config( config );
-  return config;
+  this->update_detector_parameters();
 }
 
 void
 detect_features_SURF
-::set_configuration( vital::config_block_sptr config )
+::update_detector_parameters() const
 {
-  config_block_sptr c = get_configuration();
-  c->merge_config( config );
-  p_->set_config( c );
-
 #if KWIVER_OPENCV_VERSION_MAJOR < 3
-  p_->update( detector );
+  cv::Ptr< cv_SURF_t > a = this->detector;
+  a->set( "hessianThreshold", hessian_threshold );
+  a->set( "nOctaves", n_octaves );
+  a->set( "nOctaveLayers", n_octave_layers );
+  a->set( "extended", extended );
+  a->set( "upright", upright );
 #else
   // Create a new detector rather than update on version 3.
   // Use of the update function requires a dynamic_cast which fails on Mac
-  detector = p_->create();
+  detector.constCast< cv::FeatureDetector >() = create(
+    get_hessian_threshold(), get_n_octaves(), get_n_octaves_layers(),
+    get_extended(), get_upright() );
 #endif
 }
 
@@ -176,41 +103,45 @@ detect_features_SURF
   return true;
 }
 
+// -----------------------------------------------------------------------------
+void
 extract_descriptors_SURF
-::extract_descriptors_SURF()
-  : p_( new priv )
+::initialize()
 {
   attach_logger( "arrows.ocv.SURF" );
-  extractor = p_->create();
+  extractor = create(
+    get_hessian_threshold(), get_n_octaves(), get_n_octaves_layers(),
+    get_extended(), get_upright() );
 }
 
 extract_descriptors_SURF
 ::~extract_descriptors_SURF()
 {}
 
-vital::config_block_sptr
+void
 extract_descriptors_SURF
-::get_configuration() const
+::set_configuration_internal( VITAL_UNUSED vital::config_block_sptr config )
 {
-  config_block_sptr config = extract_descriptors::get_configuration();
-  p_->update_config( config );
-  return config;
+  this->update_extractor_parameters();
 }
 
 void
 extract_descriptors_SURF
-::set_configuration( vital::config_block_sptr config )
+::update_extractor_parameters() const
 {
-  config_block_sptr c = get_configuration();
-  c->merge_config( config );
-  p_->set_config( c );
-
 #if KWIVER_OPENCV_VERSION_MAJOR < 3
-  p_->update( extractor );
+  cv::Ptr< cv_SURF_t > a = this->detector;
+  a->set( "hessianThreshold", hessian_threshold );
+  a->set( "nOctaves", n_octaves );
+  a->set( "nOctaveLayers", n_octave_layers );
+  a->set( "extended", extended );
+  a->set( "upright", upright );
 #else
   // Create a new detector rather than update on version 3.
   // Use of the update function requires a dynamic_cast which fails on Mac
-  extractor = p_->create();
+  extractor.constCast< cv::DescriptorExtractor >() = create(
+    get_hessian_threshold(), get_n_octaves(), get_n_octaves_layers(),
+    get_extended(), get_upright() );
 #endif
 }
 
