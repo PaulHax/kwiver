196027303f83ea0ebda71becbdf45b9bb98a6df8
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-02-29 16:54:07 -0500

    ocv: Adapt detect_motion_mog2

--- arrows/ocv/detect_motion_mog2.h
+++ arrows/ocv/detect_motion_mog2.h
@@ -30,24 +30,64 @@ class KWIVER_ALGO_OCV_EXPORT detect_motion_mog2
   : public vital::algo::detect_motion
 {
 public:
-  PLUGIN_INFO(
-    "ocv_mog2",
-    "OCV implementation of detect_motion using cv::BackgroundSubtractormog2" )
+  PLUGGABLE_IMPL(
+    detect_motion_mog2,
+    "OCV implementation of detect_motion using cv::BackgroundSubtractormog2",
 
-  double learning_rate = 0.01;
+    PARAM_DEFAULT(
+      var_threshold,
+      double,
+      "Threshold on the squared Mahalanobis distance between "
+      "the pixel and the model to decide whether a pixel is "
+      "well described by the background model. This parameter "
+      "does not affect the background update.",
+      36.0 ),
 
-  /// Constructor
-  detect_motion_mog2();
+    PARAM_DEFAULT(
+      history,
+      int,
+      "Length of the history.",
+      100 ),
+
+    PARAM_DEFAULT(
+      learning_rate,
+      double,
+      "determines how quickly features are “forgotten” from "
+      "histograms (range 0-1).",
+      0.01 ),
+
+    PARAM_DEFAULT(
+      blur_kernel_size,
+      int,
+      "Diameter of the normalized box filter blurring "
+      "kernel (positive integer).",
+      3 ),
+
+    PARAM_DEFAULT(
+      min_frames,
+      int,
+      "Minimum frames that need to be included in the "
+      "background model before detections are emmited.",
+      1 ),
+
+    PARAM_DEFAULT(
+      max_foreground_fract,
+      double,
+      "Specifies the maximum expected fraction of the scene "
+      "that may contain foreground movers at any time. When the "
+      "fraction of pixels determined to be in motion exceeds "
+      "this value, the background model is assumed to be "
+      "invalid (e.g., due to excessive camera motion) and is "
+      "reset. The default value of 1 indicates that no checking "
+      "is done.",
+      1 )
+  )
+
   /// Destructor
   virtual ~detect_motion_mog2() noexcept;
 
-  /// Get this algorithm's \link kwiver::vital::config_block configuration block
-  /// \endlink
-  virtual vital::config_block_sptr get_configuration() const;
-  /// Set this algorithm's properties via a config block
-  virtual void set_configuration( vital::config_block_sptr config );
   /// Check that the algorithm's configuration vital::config_block is valid
-  virtual bool check_configuration( vital::config_block_sptr config ) const;
+  bool check_configuration( vital::config_block_sptr config ) const override;
 
   /// Detect motion from a sequence of images
   ///
@@ -66,17 +106,19 @@ public:
   /// \returns A heat map image is returned indicating the confidence
   /// that motion occurred at each pixel. Heat map image is single channel
   /// and has the same width and height dimensions as the input image.
-  virtual kwiver::vital::image_container_sptr
+  kwiver::vital::image_container_sptr
   process_image(
     const kwiver::vital::timestamp& ts,
     const kwiver::vital::image_container_sptr image,
-    bool reset_model );
+    bool reset_model ) override;
 
 private:
+  void initialize() override;
+  void set_configuration_internal( vital::config_block_sptr config ) override;
   // private implementation class
   class priv;
 
-  std::unique_ptr< priv > d_;
+  KWIVER_UNIQUE_PTR( priv, d_ );
 };
 
 } // end namespace ocv
