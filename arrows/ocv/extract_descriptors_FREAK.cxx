4dc49326b5eb91c811a8e56afd61339044182119
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-02-29 16:32:38 -0500

    ocv: Adapt extract_descriptors_FREAK

--- arrows/ocv/extract_descriptors_FREAK.cxx
+++ arrows/ocv/extract_descriptors_FREAK.cxx
@@ -25,89 +25,34 @@ namespace arrows {
 
 namespace ocv {
 
-class extract_descriptors_FREAK::priv
+namespace {
+
+/// Create new cv::Ptr algo instance
+cv::Ptr< cv_FREAK_t >
+create( const extract_descriptors_FREAK& parent )
 {
-public:
-  /// Constructor
-  priv()
-    : orientation_normalized( true ),
-      scale_normalized( true ),
-      pattern_scale( 22.0f ),
-      n_octaves( 4 )
-  {}
-
-  /// Create new cv::Ptr algo instance
-  cv::Ptr< cv_FREAK_t >
-  create() const
-  {
 #if KWIVER_OPENCV_VERSION_MAJOR < 3
-    return cv::Ptr< cv_FREAK_t >(
-      new cv_FREAK_t(
-        orientation_normalized, scale_normalized, pattern_scale,
-        n_octaves )
-    );
+  return cv::Ptr< cv_FREAK_t >(
+    new cv_FREAK_t(
+      parent.get_orientation_normalized(), parent.get_scale_normalized(),
+      parent.get_pattern_scale(),
+      parent.get_n_octaves() )
+  );
 #else
-    return cv_FREAK_t::create(
-      orientation_normalized, scale_normalized,
-      pattern_scale, n_octaves );
+  return cv_FREAK_t::create(
+    parent.get_orientation_normalized(), parent.get_scale_normalized(),
+    parent.get_pattern_scale(), parent.get_n_octaves() );
 #endif
-  }
+}
 
-#if KWIVER_OPENCV_VERSION_MAJOR < 3
-  /// Update algorithm instance with current parameter values
-  void
-  update( cv::Ptr< cv_FREAK_t > freak ) const
-  {
-    freak->set( "orientationNormalized", orientation_normalized );
-    freak->set( "scaleNormalized", scale_normalized );
-    freak->set( "patternScale", pattern_scale );
-    freak->set( "nbOctave", n_octaves );
-  }
-#endif
+} // namespace
 
-  /// Set current parameter values to the given config block
-  void
-  update_config( vital::config_block_sptr& config ) const
-  {
-    config->set_value(
-      "orientation_normalized", orientation_normalized,
-      "enable orientation normalization" );
-    config->set_value(
-      "scale_normalized", scale_normalized,
-      "enable scale normalization" );
-    config->set_value(
-      "pattern_scale", pattern_scale,
-      "scaling of the description pattern" );
-    config->set_value(
-      "n_octaves", n_octaves,
-      "number of octaves covered by the detected keypoints" );
-  }
-
-  /// Set our parameters based on the given config block
-  void
-  set_config( vital::config_block_sptr const& config )
-  {
-    orientation_normalized =
-      config->get_value< bool >( "orientation_normalized" );
-    scale_normalized = config->get_value< bool >( "scale_normalized" );
-    pattern_scale = config->get_value< float >( "pattern_scale" );
-    n_octaves = config->get_value< int >( "n_octaves" );
-  }
-
-  /// Params
-  bool orientation_normalized;
-  bool scale_normalized;
-  float pattern_scale;
-  int n_octaves;
-};
-
-/// Constructor
+void
 extract_descriptors_FREAK
-::extract_descriptors_FREAK()
-  : p_( new priv )
+::initialize()
 {
   attach_logger( "arrows.ocv.FREAK" );
-  extractor = p_->create();
+  extractor = create( *this );
 }
 
 /// Destructor
@@ -115,26 +60,25 @@ extract_descriptors_FREAK
 ::~extract_descriptors_FREAK()
 {}
 
-vital::config_block_sptr
+void
 extract_descriptors_FREAK
-::get_configuration() const
+::set_configuration_internal( VITAL_UNUSED vital::config_block_sptr config )
 {
-  vital::config_block_sptr config = extract_descriptors::get_configuration();
-  p_->update_config( config );
-  return config;
+  this->update_extractor_parameters();
 }
 
 void
 extract_descriptors_FREAK
-::set_configuration( vital::config_block_sptr config )
+::update_extractor_parameters() const
 {
-  vital::config_block_sptr c = get_configuration();
-  c->merge_config( config );
-  p_->set_config( c );
 #if KWIVER_OPENCV_VERSION_MAJOR < 3
-  p_->update( extractor );
+  cv::Ptr< cv_FREAK_t > freak = extractor.dynamicCast< cv_FREAK_t >();
+  freak->set( "orientationNormalized", parent.get_orientation_normalized() );
+  freak->set( "scaleNormalized", parent.get_scale_normalized() );
+  freak->set( "patternScale", parent.get_pattern_scale() );
+  freak->set( "nbOctave", parent.get_n_octaves() );
 #else
-  extractor = p_->create();
+  extractor.constCast< cv::DescriptorExtractor >() = create( *this );
 #endif
 }
 
