58975ea066be1da6f16346aebadec0065a0386e5
Paul Beasly <paul.beasly@kitware.com>
2024-03-07 11:43:27 -0500

    Initial coding stage, not building

--- arrows/core/tests/test_filter_features.cxx
+++ arrows/core/tests/test_filter_features.cxx
@@ -5,7 +5,8 @@
 #include <arrows/core/filter_features_magnitude.h>
 #include <arrows/core/filter_features_nonmax.h>
 #include <arrows/core/filter_features_scale.h>
-#include <vital/plugin_management/plugin_manager.h>
+
+#include <test_features.h>
 
 #include <gtest/gtest.h>
 
@@ -15,11 +16,69 @@ using namespace kwiver::vital;
 using namespace kwiver::arrows::core;
 
 // ----------------------------------------------------------------------------
+// Establish constants and values for randomly generated track set
+
+// Generate a collection of features with known paramaters
+auto feature1 = std::make_shared< kwiver::vital::feature_d >(
+  Eigen::Vector2d( 12.0, 9.0 ), 15.0, 27.0, 80.0 );
+
+// Generate the feature set with default parameters
+auto input_feature_set = kwiver::testing::generate_feature_set();
+
+//
+// ----------------------------------------------------------------------------
 int
 main( int argc, char** argv )
 {
+  // Generate a feature set with the provided parameters
+  auto feature_set = kwiver::testing::generate_feature_set();
+
+  // Access the first feature from the feature set (assuming it's not empty)
+  auto features = feature_set->features();
+  if( !features.empty() )
+  {
+    auto first_feature = features[ 0 ];
+
+    // Access the magnitude attribute of the first feature
+    double magnitude = first_feature->magnitude();
+
+    // Print the magnitude attribute
+    std::cout << "Magnitude of the first feature: " << magnitude << std::endl;
+  }
+  else
+  {
+    std::cerr << "Feature set is empty!" << std::endl;
+  }
+
+  // See if I can create and access a single feature - OK
+  std::cout << "Feature 1's magnitude: " << feature1->magnitude() << '\n';
+
+  std::vector< unsigned int > indices;
+
+  // Generate instance of the filter_features_scale algorithm
+  algo::filter_features_sptr scale_algo = create_algorithm< algo::filter_features >( "scale" );
+
+  // Get the configuration of the filter_tracks algorithm
+  config_block_sptr config = scale_algo->get_configuration();
+
+  // Get the value of min_features from config block
+  double min_features = config->get_value< double >( "min_features" );
+
+  std::cout << "min features, should be 100, it is: " << min_features << '\n';
+
+  // Run the filter
+  auto filtered_set = scale_algo.filter( input_feature_set, indices );
+
+  // Output the number of features before and after filtering
+//    std::cout << "Number of features before filtering: " <<
+// input_feature_set->size() << std::endl;
+//    std::cout << "Number of features after filtering: " <<
+// filtered_feature_set->size() << std::endl;
+
   ::testing::InitGoogleTest( &argc, argv );
   return RUN_ALL_TESTS();
+
+  return 0;
 }
 
 // ----------------------------------------------------------------------------
---
+++ tests/test_features.h
@@ -0,0 +1,96 @@
+// This file is part of KWIVER, and is distributed under the
+// OSI-approved BSD 3-Clause License. See top-level LICENSE file or
+// https://github.com/Kitware/kwiver/blob/master/LICENSE for details.
+
+/**
+ * \file
+ *
+ * \brief Various functions for creating a set of features with attributes
+ * for testing the filter_features implementations
+ *
+ * These functions are shared by various tests
+ */
+
+#ifndef KWIVER_TEST_TEST_FEATURES_H_
+#define KWIVER_TEST_TEST_FEATURES_H_
+
+#include <random>
+
+#include <vital/types/track_set.h>
+
+#include <vital/types/feature.h>
+#include <vital/types/feature_set.h>
+
+#include <vital/types/track_descriptor_set.h>
+
+#include <vital/algo/filter_features.h>
+
+#include <algorithm>
+
+namespace kwiver {
+
+namespace testing {
+
+// Generate a set of generic features
+//
+// parameters are:
+// ..
+// ..
+// ..
+
+// Generate a feature set with specified parameters
+kwiver::vital::feature_set_sptr
+generate_feature_set(
+  size_t num_features = 100,
+  double loc_min_x = 0.0,
+  double loc_max_x = 100.0,
+  double loc_min_y = 0.0,
+  double loc_max_y = 100.0,
+  double mag_min = 0.0,
+  double mag_max = 10.0,
+  double scale_min = 1.0,
+  double scale_max = 2.0,
+  double angle_min = 0.0,
+  double angle_max = 180.0 )
+{
+  // Create an empty vector of features
+  std::vector< kwiver::vital::feature_sptr > features;
+
+  // Create random number generator
+  std::default_random_engine rng( std::random_device{}() );
+  std::uniform_real_distribution< double > loc_dist_x( loc_min_x, loc_max_x );
+  std::uniform_real_distribution< double > loc_dist_y( loc_min_y, loc_max_y );
+  std::uniform_real_distribution< double > mag_dist( mag_min, mag_max );
+  std::uniform_real_distribution< double > scale_dist( scale_min, scale_max );
+  std::uniform_real_distribution< double > angle_dist( angle_min, angle_max );
+
+  // Generate random features and add them to the vector
+  for( size_t i = 0; i < num_features; ++i )
+  {
+    // Generate random feature parameters
+    double loc_x = loc_dist_x( rng );
+    double loc_y = loc_dist_y( rng );
+    double mag = mag_dist( rng );
+    double scale = scale_dist( rng );
+    double angle = angle_dist( rng );
+
+    // Create a feature with the generated parameters
+    auto feature = std::make_shared< kwiver::vital::feature_d >(
+      Eigen::Vector2d( loc_x, loc_y ), mag, scale, angle );
+
+    // Add the feature to the vector
+    features.push_back( feature );
+  }
+
+  // Create a simple_feature_set from the vector of features
+  auto feature_set =
+    std::make_shared< kwiver::vital::simple_feature_set >( features );
+
+  return feature_set;
+}
+
+} // end namespace testing
+
+} // end namespace kwiver
+
+#endif
