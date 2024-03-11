4e432e9082fe2a4a65c17415e38659a570c0619b
Aron Helser <aron.helser@kitware.com>
2024-03-11 16:59:57 -0400

    Fix `tempnam` warning from gcc
    gcc produced this warning on linking:
    warning: the use of `tempnam' is dangerous, better use `mkstemp'
    
    Since this method is only used in testing, we don't worry about
    closing the file and reopening it later. We also add suffix for
    file extension typing and assume it's ok to do so.

--- tests/test_tmpfn.h
+++ tests/test_tmpfn.h
@@ -11,10 +11,13 @@
 #ifndef KWIVER_TEST_TEST_TMPFN_H_
 #define KWIVER_TEST_TEST_TMPFN_H_
 
+#include <stdexcept>
 #include <string>
+#include <vector>
 
 #include <cstdio>
 #include <cstdlib>
+#include <unistd.h>
 
 #ifdef _WIN32
 #define tempnam( d, p ) _tempnam( d, p )
@@ -29,16 +32,29 @@ namespace testing {
 /** @brief Generate a unique file name in the current working directory.
  *
  * @param prefix Prefix for generated file name.
- * @param suffix Suffix for generated file name.
+ * @param suffix Suffix for generated file name, ignored.
  */
 std::string
 temp_file_name( char const* prefix, char const* suffix )
 {
-  auto const n = tempnam( ".", prefix );
-  auto const s = std::string( n );
-  free( n );
+  // to use mkstemp, the template must have 6 X's at the end, so we ignore the
+  // suffix
+  std::string templateName = "./" + std::string( prefix ) + "XXXXXX";
+  std::vector< char > tempFileName( templateName.begin(), templateName.end() );
+  tempFileName.push_back( '\0' );
 
-  return s + suffix;
+  int fd = mkstemp( tempFileName.data() );
+  if( fd == -1 )
+  {
+    throw std::runtime_error( "Failed to create temporary file" );
+  }
+
+  close( fd );
+
+  // add the suffix back, because some files are typed based on suffix.
+  std::string result( tempFileName.data() );
+
+  return result + suffix;
 }
 
 } // end namespace testing
