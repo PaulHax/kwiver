2358225a9d0cf1c475d121b3fb652e764be10d76
Yohann Bearzi <yohann.bearzi@kitware.com>
2024-03-24 13:33:24 -0400

    cleaned vxl::bundle_adjust for kwiver 2

--- arrows/vxl/tests/test_bundle_adjust.cxx
+++ arrows/vxl/tests/test_bundle_adjust.cxx
@@ -33,6 +33,57 @@ TEST ( bundle_adjust, create )
 }
 
 // ----------------------------------------------------------------------------
+TEST ( bundle_adjust, default_config )
+{
+  EXPECT_PLUGGABLE_IMPL(
+    bundle_adjust,
+    "Use VXL (vpgl) to bundle adjust cameras and landmarks.",
+    PARAM_DEFAULT(
+      verbose, bool,
+      "If true, write status messages to the terminal showing "
+      "optimization progress at each iteration",
+      false ),
+    PARAM_DEFAULT(
+      use_m_estimator, bool,
+      "If true, use a M-estimator for a robust loss function. "
+      "Currently only the Beaton-Tukey loss function is supported.",
+      false ),
+    PARAM_DEFAULT(
+      m_estimator_scale, double,
+      "The scale of the M-estimator, if enabled, in pixels. "
+      "Inlier landmarks should project to within this distance "
+      "from the feature point.",
+      1 ),
+    PARAM_DEFAULT(
+      estimate_focal_length, bool,
+      "If true, estimate a shared intrinsic focal length for all "
+      "cameras.  Warning: there is often a depth/focal length "
+      "ambiguity which can lead to long optimizations.",
+      false ),
+    PARAM_DEFAULT(
+      normalize_data, bool,
+      "Normalize the data for numerical stability. "
+      "There is no reason not enable this option, except "
+      "for testing purposes.",
+      true ),
+    PARAM_DEFAULT(
+      max_iterations, unsigned,
+      "Termination condition: maximum number of LM iterations",
+      1000 ),
+    PARAM_DEFAULT(
+      x_tolerance, double,
+      "Termination condition: Relative change is parameters. "
+      "Exit when (mag(delta_params) / mag(params) < x_tol).",
+      1e-08 ),
+    PARAM_DEFAULT(
+      g_tolerance, double,
+      "Termination condition: Maximum gradient magnitude. "
+      "Exit when (max(grad_params) < g_tol)",
+      1e-08 )
+  );
+}
+
+// ----------------------------------------------------------------------------
 #include <arrows/tests/test_bundle_adjust.h>
 
 // ----------------------------------------------------------------------------
