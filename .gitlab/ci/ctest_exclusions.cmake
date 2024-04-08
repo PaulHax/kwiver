0b65e58d593de4ae3707ac5cf6a0be1beecec1ff
Ben Boeckel <ben.boeckel@kitware.com>
2024-04-08 16:05:20 -0400

    ci: exclude GoogleTest tests
    See: #7

--- .gitlab/ci/ctest_exclusions.cmake
+++ .gitlab/ci/ctest_exclusions.cmake
@@ -1,5 +1,21 @@
 set(test_exclusions)
 
+if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows")
+  list(APPEND test_exclusions
+    # Some degenerate behavior in GoogleTest support.
+    "^>$"
+
+    # GoogleTest tests do not initialize Python properly. Exclude them pending
+    # further investigation. See #7
+    "^(ceres|core|ffmpeg|gdal|klv|mvg|ocv|pdal|qt|serialize-json|super3d|vital|vtk|vxl):"
+    # This C++ test also doesn't initialize it properly.
+    "^CXX::demo_python_impl_call$"
+
+    # Plugins aren't found. Needs investigation.
+    "^test-python-test_image_io$"
+    )
+endif ()
+
 # format:
 # Add one per line:
 # "^name_of_test$"
