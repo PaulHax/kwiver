711062bce593f619a3a176e141ec798065805656
Christos Tsolakis <christos.tsolakis@kitware.com>
2024-04-12 17:28:34 -0400

    ci: update windows exclusions

--- .gitlab/ci/ctest_exclusions.cmake
+++ .gitlab/ci/ctest_exclusions.cmake
@@ -2,17 +2,14 @@ set(test_exclusions)
 
 if ("$ENV{CMAKE_CONFIGURATION}" MATCHES "windows")
   list(APPEND test_exclusions
-    # Some degenerate behavior in GoogleTest support.
-    "^>$"
+    # See issue #9
+    "^qt:image_io.type<struct image_type<unsigned char,1,24>>$"
+    "^qt:image_io.type<struct image_type<unsigned char,3,13>>$"
+    "^qt:image_io.type<struct image_type<bool,1,1>>$"
 
-    # GoogleTest tests do not initialize Python properly. Exclude them pending
-    # further investigation. See #7
-    "^(ceres|core|ffmpeg|gdal|klv|mvg|ocv|pdal|qt|serialize-json|super3d|vital|vtk|vxl):"
-    # This C++ test also doesn't initialize it properly.
-    "^CXX::demo_python_impl_call$"
-
-    # Plugins aren't found. Needs investigation.
-    "^test-python-test_image_io$"
+    # See issue #10
+    "^ffmpeg:ffmpeg_video_output.round_trip$"
+    "^ffmpeg:ffmpeg_video_output.round_trip_direct$"
     )
 endif ()
 
